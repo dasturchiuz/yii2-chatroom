6e7bfefd5336544db84980a7f5d90d67830ccdd8
dasturchiuz <dasturchiuz@gmail.com>
2019-02-26 22:18:13 +0500

    TEST4

---
+++ ChatRoom.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: loock
+ * Date: 2/26/19
+ * Time: 10:16 PM
+ */
+
+namespace dasturchiuz\chatroom;
+
+
+class ChatRoom
+{
+    public function salom(){
+        echo "Salom";
+    }
+}
